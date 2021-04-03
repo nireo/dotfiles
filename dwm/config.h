fa0f6e65f0463e607f3fde05e8b4aa616481a487
nireo <eemil.lehtikangas1@gmail.com>
2021-04-03 19:11:46 +0300

    added a my doom configuration

--- dwm/config.h
+++ dwm/config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov = 10;  /* vert outer gap between windows and sc
 static const int smartgaps = 0;         /* 1 means no outer gap when there is only one window */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Hack Nerd Font:size=9"};
-static const char dmenufont[] = "Hack Nerd Font:size=9";
+static const char *fonts[] = {"Iosevka:size=9"};
+static const char dmenufont[] = "Iosevka:size=9";
 static const char col_gray1[] = "#282828";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
