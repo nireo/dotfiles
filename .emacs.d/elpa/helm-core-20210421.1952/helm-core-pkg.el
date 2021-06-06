e7d06ce498b6df98bdf23e6758b5df3d851a8156
nireo <eemil.lehtikangas1@gmail.com>
2021-06-06 20:42:01 +0300

    removed emacs configuration from here and updated dwm and dwmblocks

--- .emacs.d/elpa/helm-core-20210421.1952/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20210421.1952" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "d543122292c14b34a861364200791f1a70fbdce3")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
