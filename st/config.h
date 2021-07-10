2d85fce002aa1ab57db9feaa3650b33b85032240
nireo <eemil.lehtikangas1@gmail.com>
2021-07-10 16:10:36 +0300

    switched to using xmonad from dwm

--- st/config.h
+++ st/config.h
@@ -138,7 +138,7 @@ unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 6;
+static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
