6d5927e55dc52645b031073aa6833f16bf6f7317
nireo <eemil.lehtikangas1@gmail.com>
2021-10-31 17:05:29 +0200

    added a i3 config

--- dwm/config.h
+++ dwm/config.h
@@ -3,20 +3,23 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 10;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;  /* vert inner gap between windows */
-static const unsigned int gappoh = 10;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov = 10;  /* vert outer gap between windows and screen edge */
-static const int smartgaps = 1;         /* 1 means no outer gap when there is only one window */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 0;            /* 0 means bottom bar */
+static const unsigned int gappih = 0;   /* horiz inner gap between windows */
+static const unsigned int gappiv = 0;   /* vert inner gap between windows */
+static const unsigned int gappoh =
+    0; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    0; /* vert outer gap between windows and screen edge */
+static const int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {"Monospace:size=10"};
 static const char dmenufont[] = "Monospace:size=10";
-static const char col_gray1[] = "#2a2527";
-static const char col_gray2[] = "#2a2527";
-static const char col_gray3[] = "#d9bb80";
-static const char col_gray4[] = "#e6d6ac";
-static const char col_cyan[] = "#242021";
+static const char col_gray1[] = "#1d1f21";
+static const char col_gray2[] = "#1d1f21";
+static const char col_gray3[] =  "#7799bb";
+static const char col_gray4[] = "#c5c9c6";
+static const char col_cyan[] = "#151515";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
@@ -24,7 +27,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"(1:www)", "(2:dev)", "(3:chat)", "(4:other)"};
+static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7"};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -37,9 +40,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.50;  /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;     /* number of clients in master area */
-static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
+static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;    /* number of clients in master area */
+static const int resizehints =
+    1; /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -50,28 +54,29 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
-#define TAGKEYS(KEY, TAG)                                                                             \
-    {MODKEY, KEY, view, {.ui = 1 << TAG}}, {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-        {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                                             \
-        {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+#define TAGKEYS(KEY, TAG)                                                      \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                     \
-    {                                  \
-        .v = (const char *[]) {        \
-            "/bin/sh", "-c", cmd, NULL \
-        }                              \
-    }
+#define SHCMD(cmd)                                                             \
+  {                                                                            \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+  }
 
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-/* static const char *dmenucmd[] = {"dmenu_run", "-m",      dmenumon, "-fn",     dmenufont, "-nb",
- * col_gray1, */
+static char dmenumon[2] =
+    "0"; /* component of dmenucmd, manipulated in spawn() */
+/* static const char *dmenucmd[] = {"dmenu_run", "-m",      dmenumon, "-fn",
+ * dmenufont, "-nb", col_gray1, */
 /*																 "-nf",
  * col_gray3, "-sb",    col_gray1, "-sf",     col_gray4, NULL}; */
-static const char *dmenucmd[] = {"dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-                                 "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"alacritty", NULL};
 static const char *emacs[] = {"emacs", NULL};
 static const char *qutebrowser[] = {"qutebrowser", NULL};
 
@@ -118,8 +123,9 @@ static Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5)
-        TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_e, quit, {0}},
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_e, quit, {0}},
 };
 
 /* button definitions */
