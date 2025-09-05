71309a23ca4ad05dbc241e739fb069657ba1b8c9
nireo <eemil.lehtikangas1@gmail.com>
2025-09-05 23:38:40 +0300

    added a small script to track zathura sandbox pages in a easy way

--- clear_inputs.js
+++ clear_inputs.js
@@ -1,17 +1,17 @@
-// My university site doesn't contain any way to clear done exercises so to practice for 
+// My university site doesn't contain any way to clear done exercises so to practice for
 // exam clear each field forcefully.
-document.querySelectorAll('input').forEach(input => {
-  if (input.type === 'checkbox' || input.type === 'radio') {
+document.querySelectorAll("input").forEach((input) => {
+  if (input.type === "checkbox" || input.type === "radio") {
     input.checked = false;
-  } else if (input.type !== 'submit' && input.type !== 'button') {
-    input.value = '';
+  } else if (input.type !== "submit" && input.type !== "button") {
+    input.value = "";
   }
 });
 
-document.querySelectorAll('textarea').forEach(textarea => {
-  textarea.value = '';
+document.querySelectorAll("textarea").forEach((textarea) => {
+  textarea.value = "";
 });
 
-document.querySelectorAll('select').forEach(select => {
+document.querySelectorAll("select").forEach((select) => {
   select.selectedIndex = 0;
 });
